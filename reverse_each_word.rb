2bdaadf9e4bb2277c707e0215e3c5ba30653ecab
Amanda D <amanda@depaula.xyz>
2020-09-24 20:04:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(string)
-my_Array = string.split(" ")
-second_Array = my_Array.collect {|i| i.reverse}
-second_Array.join(' ')
+my_Array = string.split
+my_Array.collect {|i| i.reverse}.join(' ')
 end
